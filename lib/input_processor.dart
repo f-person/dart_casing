3e771d808545fe17e20305cb20bb0eff51ee41c5
f-person <agakaryan.arshak@gmail.com>
2020-03-08 23:05:55 +0400

    Don't make the input lowercase in order to have true camel case

--- lib/input_processor.dart
+++ lib/input_processor.dart
@@ -1,35 +1,29 @@
-﻿class InputProcessor
-{
-    static final RegExp _symbols = RegExp(r'[ ./_\-\\]');
+﻿class InputProcessor {
+  static final RegExp _symbols = RegExp(r'[ ./_\-\\]');
 
-    static List<String> getWordsGroup(String input)
-    {
-        final lowerInput = input.toLowerCase();
-        final inputLength = lowerInput.length;
-        final buffer = StringBuffer();
-        List<String> group = [];
+  static List<String> getWordsGroup(String input) {
+    final inputLength = input.length;
+    final buffer = StringBuffer();
+    List<String> group = [];
 
-        for (int i = 0; i < inputLength; i++)
-        {
-            String currentChar = lowerInput[i];
-            String nextChar = i == inputLength - 1 ? null : lowerInput[i + 1];
+    for (int i = 0; i < inputLength; i++) {
+      String currentChar = input[i];
+      String nextChar = i == inputLength - 1 ? null : input[i + 1];
 
-            if (_symbols.hasMatch(currentChar)) continue;
+      if (_symbols.hasMatch(currentChar)) continue;
 
-            buffer.write(currentChar);
+      buffer.write(currentChar);
 
-            if (isEndOfWord(nextChar))
-            {
-                group.add(buffer.toString());
-                buffer.clear();
-            }
-        }
-
-        return group;
+      if (isEndOfWord(nextChar)) {
+        group.add(buffer.toString());
+        buffer.clear();
+      }
     }
 
-    static bool isEndOfWord(String nextChar)
-    {
-       return nextChar == null || _symbols.hasMatch(nextChar);
-    }
+    return group;
+  }
+
+  static bool isEndOfWord(String nextChar) {
+    return nextChar == null || _symbols.hasMatch(nextChar);
+  }
 }
