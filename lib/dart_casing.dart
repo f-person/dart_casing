a85b671b72fb3c31433b5c0e13ea235b7b1d18ae
f-person <agakaryan.arshak@gmail.com>
2020-03-08 23:02:24 +0400

    Don't make the input lowercase in order to keep true camel case, Format the codebase

--- lib/dart_casing.dart
+++ lib/dart_casing.dart
@@ -2,73 +2,62 @@ library dart_casing;
 
 import 'package:dart_casing/input_processor.dart';
 
-class Casing
-{
-    static String camelCase(String input)
-    {
-        final group = InputProcessor.getWordsGroup(input);
-        final buffer = StringBuffer();
+class Casing {
+  static String camelCase(String input) {
+    final group = InputProcessor.getWordsGroup(input);
+    final buffer = StringBuffer();
 
-        for(int i = 0; i < group.length; i++)
-        {
-            var word = group[i];
+    for (int i = 0; i < group.length; i++) {
+      var word = group[i];
 
-            buffer.write(i == 0 ? word : _uppercaseFirst(word));
-        }
-
-        return buffer.toString();
+      buffer.write(i == 0 ? word : _uppercaseFirst(word));
     }
 
-    static String pascalCase(String input)
-    {
-        final group = InputProcessor.getWordsGroup(input).map(_uppercaseFirst);
+    return buffer.toString();
+  }
 
-        return group.join();
-    }
+  static String pascalCase(String input) {
+    final group = InputProcessor.getWordsGroup(input).map(_uppercaseFirst);
 
-    static String snakeCase(String input)
-    {
-        return lowerCase(input, separator: "_");
-    }
+    return group.join();
+  }
 
-    static String paramCase(String input)
-    {
-        return lowerCase(input, separator: "-");
-    }
+  static String snakeCase(String input) {
+    return lowerCase(input, separator: "_");
+  }
 
-    static String dotCase(String input)
-    {
-        return lowerCase(input, separator: ".");
-    }
+  static String paramCase(String input) {
+    return lowerCase(input, separator: "-");
+  }
 
-    static String constantCase(String input)
-    {
-        return upperCase(input, separator: "_");
-    }
+  static String dotCase(String input) {
+    return lowerCase(input, separator: ".");
+  }
 
-    static String titleCase(String input, {String separator = " "})
-    {
-        final group = InputProcessor.getWordsGroup(input).map(_uppercaseFirst);
+  static String constantCase(String input) {
+    return upperCase(input, separator: "_");
+  }
 
-        return group.join(separator);
-    }
+  static String titleCase(String input, {String separator = " "}) {
+    final group = InputProcessor.getWordsGroup(input).map(_uppercaseFirst);
 
-    static String lowerCase(String input, {String separator = " "})
-    {
-        final group = InputProcessor.getWordsGroup(input);
+    return group.join(separator);
+  }
 
-        return group.join(separator);
-    }
+  static String lowerCase(String input, {String separator = " "}) {
+    final group = InputProcessor.getWordsGroup(input);
 
-    static String upperCase(String input, {String separator = " "})
-    {
-        final group = InputProcessor.getWordsGroup(input).map((x) => x.toUpperCase());
+    return group.join(separator);
+  }
 
-        return group.join(separator);
-    }
+  static String upperCase(String input, {String separator = " "}) {
+    final group =
+        InputProcessor.getWordsGroup(input).map((x) => x.toUpperCase());
 
-    static String _uppercaseFirst(String word)
-    {
-        return word.replaceRange(0, 1, word[0].toUpperCase());
-    }
+    return group.join(separator);
+  }
+
+  static String _uppercaseFirst(String word) {
+    return word.replaceRange(0, 1, word[0].toUpperCase());
+  }
 }
